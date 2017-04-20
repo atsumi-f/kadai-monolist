77b9b8fe0e30329078e01d9b044b7e5aa9533f83
atsumi-f <kamaitachi8489@gmail.com>
2017-04-20 03:47:12 +0000

    Have ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,4 +3,9 @@ class RankingsController < ApplicationController
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
   end
+  
+  def hafe
+    @ranking_counts = Hafe.ranking
+    @items = Item.find(@ranking_counts.keys)
+  end
 end
